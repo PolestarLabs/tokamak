17054787cccc88b73365abb897e50b65f5f7b65e
DanielaGC <danielagc676@gmail.com>
2021-01-10 22:21:12 -0300

    Revert "TOMAR TUDO NO CU"
    This reverts commit 4a1b20a1aa06946dfc52971bda96f4ad7a87544b.

--- src/server/server.go
+++ src/server/server.go
@@ -1,43 +1,43 @@
 package server
 
 import (
-	"github.com/gofiber/fiber/v2"
-	"image"
-	"image/png"
-	"tokamak/src/generator"
-	"tokamak/src/generator/profile"
+  "github.com/gofiber/fiber/v2"
+  "tokamak/src/generator"
+  "image"
+  "image/png"
+  "tokamak/src/generator/profile"
 )
 
-func StartServer(port string) {
-	app := fiber.New()
-	gen := generator.NewGenerator()
-	encoder := png.Encoder{
-		CompressionLevel: -1, // no compression.
-	}
-
-	app.Get("/version", func(c *fiber.Ctx) error {
-		return c.SendString("tokamak v1 (fasthttp/fiber; golang)")
-	})
-
-	app.Post("/render/profile", func(c *fiber.Ctx) error {
-		p := new(profilegenerator.ProfileData)
-
-		if err := c.BodyParser(p); err != nil {
-			return err
-		}
-
-		var img image.Image
-		c.Set("Content-Type", "image/png")
-
-		switch p.Type {
-		case "default":
-			img = profilegenerator.RenderDefaultProfile(gen, p)
-		default:
-			img = profilegenerator.RenderDefaultProfile(gen, p)
-		}
-
-		return encoder.Encode(c.Context(), img)
-	})
-
-	_ = app.Listen(":" + port)
+func StartServer (port string) {
+  app := fiber.New()
+  gen := generator.NewGenerator()
+  encoder := png.Encoder {
+    CompressionLevel: -1, // no compression.
+  }
+  
+  app.Get("/version", func(c *fiber.Ctx) error {
+    return c.SendString("tokamak v1 (fasthttp/fiber; golang)")
+  })
+  
+  app.Post("/render/profile", func (c *fiber.Ctx) error {
+    p := new(profilegenerator.ProfileData)
+    
+    if err := c.BodyParser(p); err != nil {
+      return err
+    }
+    
+    var img image.Image
+    c.Set("Content-Type", "image/png")
+    
+    switch p.Type {
+      case "default":
+        img = profilegenerator.RenderDefaultProfile(gen, p)
+      default:
+        img = profilegenerator.RenderDefaultProfile(gen, p)
+    }
+    
+    return encoder.Encode(c.Context(), img)
+  })
+  
+  app.Listen(":" + port)
 }
