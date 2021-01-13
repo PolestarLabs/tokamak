bc4f2ad2511da27f52d78d76aaa51d19851e4f79
blueslimee <blueslimexdfodeu@gmail.com>
2021-01-13 14:55:11 -0400

    add laranjo image gen

---
+++ src/generator/misc/laranjo.go
@@ -0,0 +1,24 @@
+package miscgenerator
+
+import (
+  "image"
+  "tokamak/src/generator"
+  "github.com/fogleman/gg"
+)
+
+type LaranjoData struct {
+  Text string `json:"text" form:"text"`
+}
+
+func RenderLaranjoImage(g generator.Generator, p *LaranjoData) image.Image {
+  dc := gg.NewContext(685, 494)
+  img := g.Toolbox.GetAsset("foundation/laranjo")
+  dc.DrawImage(img, 0, 0)
+  
+  dc.LoadFontFace("../assets/fonts/Poppins/Poppins-Medium.ttf", 25)
+  dc.SetHexColor("000000")
+  
+  g.Toolbox.DrawTextWrapped(dc, p.Text, 20, 30, 655, 800, 26)
+
+  return dc.Image()
+}
