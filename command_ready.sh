506ebd312a6ceba8326155b853c1e554d3d01991
nayv <navycakedeveloper@gmail.com>
2021-11-18 03:21:00 -0300

    Small bash and simple for development.

---
+++ command_ready.sh
@@ -0,0 +1,8 @@
+echo "This is a quick command to facilitate application development."
+echo "If you found any bugs please report them on this Github. Thanks for reading <3."
+echo "Github: https://github.com/RabbitHouseCorp/tokamak"
+echo "Issues: https://github.com/RabbitHouseCorp/tokamak/issues"
+echo "RabbitHouseCorp: https://github.com/RabbitHouseCorp"
+echo 
+echo 
+cd ./src && echo "[1/3] Running GO GET" && go get  && echo "[2/3] Install..." && go install && echo "[3/3] Ready! Tokamak ready."   && go run main.go
